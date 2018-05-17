4da721c46179961a0e151749dd512930f8a3b233
Chris Daley <chebizarro@gmail.com>
2018-05-17 13:43:03 -0700

    added Destroy and LoadURL functions to Style

--- mbgl/style.go
+++ mbgl/style.go
@@ -5,10 +5,23 @@ package mbgl
 */
 import "C"
 
+import "unsafe"
+
 type Style struct {
 	cptr uintptr
 }
 
 func (s Style) cPtr() uintptr {
 	return s.cptr
+}
+
+func (s Style) Destroy() {
+	C.mbgl_style_destroy(C.MbglStyle(s.cptr))
+}
+
+func (s Style) LoadURL(url string) {
+	curl := C.CString(url)
+	defer C.free(unsafe.Pointer(curl))
+
+	C.mbgl_style_load_url(C.MbglStyle(s.cptr), curl)
 }
