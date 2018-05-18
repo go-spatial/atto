f7fb2f9c7f36e8b3898358faae78d75339f58da6
Chris Daley <chebizarro@gmail.com>
2018-05-18 12:33:31 -0700

    added flags to main package

--- atto.go
+++ atto.go
@@ -1,35 +1,45 @@
 package main
 
 import (
-	//"os"
+	"flag"
 	"runtime"
 	
-	//"github.com/jung-kurt/gofpdf"
-
 	"github.com/go-spatial/atto/mbgl"
 )
 
 func main() {
+
+    debugFlag := flag.Bool("debug", false, "Output debugging information")
+    pixelRatioFlag := flag.Float64("r", 1, "Pixel ratio for the generated image")
+    widthFlag := flag.Uint("w", 512, "Width in pixels of the generated image")
+    heightFlag := flag.Uint("h", 512, "Height in pixels of the generated image")
+	baseUrlFlag := flag.String("url", "https://osm.tegola.io/", "Url of the Mapbox Vector Tile server")
+	styelUrlFlag := flag.String("style", "https://raw.githubusercontent.com/go-spatial/tegola-web-demo/master/styles/camo.json" , "Url of the style to use to render the image")
+    zoomFlag := flag.Float64("z", 1, "Zoom level for the generated image")
+    pitchFlag := flag.Float64("p", 0, "Pitch for the generated image")
+    bearingFlag := flag.Float64("b", 0, "Bearing for the generated image")
+    latFlag := flag.Float64("lat", 39.153, "Latitude for the generated image")
+    lngFlag := flag.Float64("lng", -76.275, "Longtitude for the generated image")
+    outputFlag := flag.String("o", "out.png", "File name for the generated image")
     
-    //debug := true
+    flag.Parse()
     
-    var pixelRatio float32 = 5
-    
+    pixelRatio := float32(*pixelRatioFlag)
+    zoom := float32(*zoomFlag)
+    bearing := float32(*bearingFlag)
+    pitch := float32(*pitchFlag)        
+
     loop := mbgl.NewRunLoop()
     defer loop.Destroy()
-    
-	//token := os.Getenv("MAPBOX_ACCESS_TOKEN")
-	
-    //fileSource := mbgl.NewDefaultFileSource("mbgl/testdata/cache.sqlite", ".")
-    //fileSource.SetAccessToken(token)
+
     fileSource := mbgl.NewOnlineFileSource()
-    fileSource.SetAPIBaseUrl("https://osm.tegola.io/")
+    fileSource.SetAPIBaseUrl(*baseUrlFlag)
     defer fileSource.Destroy()
 
     threadPool := mbgl.NewThreadPool(runtime.NumCPU())
     defer threadPool.Destroy()
-    
-    size := mbgl.Size{ 1024, 1024 }
+
+    size := mbgl.Size{ uint32(*widthFlag), uint32(*heightFlag) }
     
     frontEnd := mbgl.NewHeadlessFrontend(size, pixelRatio, fileSource, threadPool)
     defer frontEnd.Destroy()
@@ -37,22 +47,18 @@ func main() {
     pmap := mbgl.NewMap(frontEnd, size, pixelRatio, fileSource, threadPool, mbgl.Static, mbgl.HeightOnly, mbgl.Default)
     defer pmap.Destroy()
     
-    pmap.GetStyle().LoadURL("https://raw.githubusercontent.com/go-spatial/tegola-web-demo/master/styles/camo.json")
+    pmap.GetStyle().LoadURL(*styelUrlFlag)
     
-    latLng := mbgl.NewLatLng(39.153,-76.275)
+    latLng := mbgl.NewLatLng(float32(*latFlag),float32(*lngFlag))
     defer latLng.Destroy()
-    
-    var zoom, bearing, pitch float32 = 8, 0, 0
-    
+        
     pmap.SetLatLngZoom(latLng, zoom)
     pmap.SetBearing(bearing)
     pmap.SetPitch(pitch)
     
-	//if debug {
-	//	pmap.SetDebug()
-    //}
+	if *debugFlag {
+		pmap.SetDebug(mbgl.TileBorders | mbgl.ParseStatus)
+    }
     
-    frontEnd.RenderToFile(pmap, "out.png")
-    
-    //image := EncodePNG(frontEnd.Render(pmap))
+    frontEnd.RenderToFile(pmap, *outputFlag)
 }
